c8e0200e229ce66cd19a0dde9ee945ff1d38b13c
enzolepoul <contact.enzolp@gmail.com>
2026-02-11 10:56:35 -0500

    feat: simplification des vars dans lab2

--- menu2/src/LaboSelections2.java
+++ menu2/src/LaboSelections2.java
@@ -2,10 +2,6 @@ public class LaboSelections2 {
     public static void main(String[] args) throws Exception {
         int nombreEntierSaisi;
 
-        boolean nombreSaisiPair;
-        boolean nombreSaisiMultipleDe10;
-        int chiffresDansNombreSaisi;
-
         System.out.print("Veuillez entrer un nombre entier : ");
 
         nombreEntierSaisi = Clavier.lireInt();
@@ -14,17 +10,13 @@ public class LaboSelections2 {
             System.out.print("Nombre refusé! ");
             System.exit(0);
         } else {
-            nombreSaisiPair = nombreEntierSaisi % 2 == 0;
-            nombreSaisiMultipleDe10 = nombreEntierSaisi % 10 == 0;
-            chiffresDansNombreSaisi = Integer.toString(nombreEntierSaisi).length();
+            System.out.println((nombreEntierSaisi % 2 == 0) ? "Le nombre saisi est pair" : "Le nombre saisi est impair");
 
-            System.out.println((nombreSaisiPair == true) ? "Le nombre saisi est pair" : "Le nombre saisi est impair");
-
-            if (nombreSaisiMultipleDe10) {
+            if (nombreEntierSaisi % 10 == 0) {
                 System.out.println("Le nombre saisi est un multiple de 10");
             }
 
-            System.out.println("Le nombre saisi a " + chiffresDansNombreSaisi + " chiffres");
+            System.out.println("Le nombre saisi a " + (Integer.toString(nombreEntierSaisi).length()) + " chiffres");
         }
     }
 }
